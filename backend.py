36c0b3fbb0a96f71f9ed24828705242bc32c4ac9
ABISHA S <157133720+Abisha9328@users.noreply.github.com>
2025-07-10 00:20:08 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "QR Code Generator Backend is running."}
+
+@app.get("/api/echo")
+def echo_url(url: str = Query(..., description="The original URL to echo")):
+    return {"original_url": url}
